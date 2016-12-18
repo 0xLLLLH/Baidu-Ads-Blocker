4daba0c688c5831b5d5a10b3070b43836a031e25
0xLLLLH <linlihao159@gmail.com>
2016-12-19 00:47:43 +0800

    重写了弹出页面,不再使用Vue.js

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,76 @@
-var vm = new Vue({
-            el: "body",
-            data: {
-                blockSidebar: true
-            },
-            created:function() {
-            	var _this = this;
-            	chrome.storage.local.get(["blockSidebar"],function(items){
-            		_this.blockSidebar = items.blockSidebar;
-            		console.log(items);
-            	});
-            },
-            watch:{
-            	blockSidebar: function () {
-            		chrome.storage.local.set({"blockSidebar":this.blockSidebar},function(){
-            			console.log("blockSidebar: "+this.blockSidebar);
-            		});
-            	}
-            }
+/**
+ * popup.js - Scripts of popup.html
+ */
+const STORAGE_KEY = 'AdsBlockerOptions';
+
+let container = document.querySelector('.container');
+
+let options = [
+    {
+        name: "active",
+        desc: "启用插件",
+        value: true
+    },
+    {
+        name: "blockSidebar",
+        desc: "屏蔽搜索结果页右侧推荐",
+        value: true
+    }
+];
+
+function getOptions() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get([`${STORAGE_KEY}`], function(items){
+            resolve(items[STORAGE_KEY]);
         });
+    });
+}
+
+function setOptions() {
+    chrome.storage.local.set({
+        [STORAGE_KEY]: options
+    }, () => {
+        console.log('Options saved.');
+    });
+}
+
+function renderOptions() {
+    let optHtml = options.map((opt, idx) => (
+        `
+        <div class="menu-block">
+            ${opt.desc}
+            <label class="ui-switch">
+                <input type="checkbox"
+                    data-option-name="${opt.name}" data-index="${idx}"
+                    ${opt.value?'checked':''}>
+            </label>
+        </div>
+        `
+    )).join('');
+
+    container.innerHTML = container.innerHTML + optHtml;
+}
+
+function bindEvent() {
+    let inputs = Array.from(container.querySelectorAll('input[type="checkbox"]'));
+    inputs.map((checkbox) => {
+        checkbox.addEventListener('change', (e) => {
+            let optionName = checkbox.dataset.optionName;
+            let optionValue = checkbox.checked;
+            let optionIndex = checkbox.dataset.index;
+
+            console.log('option "%s" now change to "%s"', optionName, optionValue);
+
+            options[optionIndex].value = optionValue;
+
+            setOptions();
+        });
+    });
+}
+
+getOptions().then((savedOptions) => {
+    options = savedOptions || options;
+
+    renderOptions();
+    bindEvent();
+});
